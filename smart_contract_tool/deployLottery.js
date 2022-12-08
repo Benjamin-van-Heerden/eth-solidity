aa9469226796eae1de3f96ff158c0fa8b8041631
Benjamin-van-Heerden <benjaminvh1997@gmail.com>
2022-12-08 23:51:29 +0200

    pretty much done with lattery webapp.

--- smart_contract_tool/deployLottery.js
+++ smart_contract_tool/deployLottery.js
@@ -1,13 +1,23 @@
 import HDWalletProvider from "@truffle/hdwallet-provider";
 import { ethers } from "ethers";
-import { inboxAbi, inboxBytecode } from "./compile.js";
+import { lotteryAbi, lotteryBytecode } from "./compile.js";
 import dotenv from "dotenv";
 dotenv.config();
 
-
-const chainProvider = new HDWalletProvider(
-    process.env.WALLET_PASSPHRASE,
-    process.env.INFURA_URL
-);
+const chainProvider = new HDWalletProvider(process.env.WALLET_PASSPHRASE, process.env.INFURA_URL);
 
 const ethProvider = new ethers.providers.Web3Provider(chainProvider);
+
+(async function deployLottery() {
+	const accounts = await ethProvider.listAccounts();
+	console.log("Attempting to deploy from account:", accounts[0]);
+	const signer = ethProvider.getSigner(accounts[0]);
+
+	// deploy the contract with 1000000 gas
+	const factory = new ethers.ContractFactory(lotteryAbi, lotteryBytecode, signer);
+	const lotteryContract = await factory.deploy({ gasLimit: 1000000 });
+	console.log("Contract deployed to:", lotteryContract.address);
+	console.log("Lottery contract:", lotteryContract);
+})();
+
+chainProvider.engine.stop();
